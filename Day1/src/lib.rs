4933bac8d682a54f635a8811c65b4a984e5425e3
Justin Lowen <Justin.G.Lowen@gmail.com>
2022-12-07 23:32:18 -0600

    Day 1-Part 2: Success - fighting type checker / casting
    Several differences from Python. Blocks use Braces and not colons like
    Python.
    Indexing is start_idx..end_idx, not inclusive on RHS
    and not seprated by a colon :.
    Coercion is not a thing, so casting is much
    more common even for comparing vector
    length usize to i32!

--- Day1/src/lib.rs
+++ Day1/src/lib.rs
@@ -3,14 +3,20 @@ use std::fs;
 use std::process;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("Hello, world!");
     let input = get_input(&config.file_path).unwrap_or_else(|err| {
         println!("Application error: {}", err);
         process::exit(1)
     });
+    let operation = &config.operation;
     let elves = parse_list_of_lists(input);
-    let max_sum = find_max_sum(elves);
-    println!("{}", max_sum);
+
+    let result;
+    if operation == "max" {
+        result = find_max_sum(elves);
+    } else {
+        result = find_sum_of_largest_n_sums(elves, &config.n_groups);
+    }
+    println!("{}", result);
 
     Ok(())
 }
@@ -47,6 +53,7 @@ pub fn parse_list_of_lists(string: String) -> Vec<Vec<i32>> {
             food_calories.push(calories);
         }
     }
+    elves.push(food_calories.clone());
     return elves
 }
 
@@ -64,8 +71,37 @@ pub fn find_max_sum(input: Vec<Vec<i32>>) -> i32 {
     return max_sum
 }
 
+pub fn find_sum_of_largest_n_sums(two_dim_vec: Vec<Vec<i32>>, n_groups: &i32) -> i32 {
+    let mut group_sums = Vec::new();
+    println!("{}", two_dim_vec.len());
+    for inner_list in two_dim_vec.iter() {
+        let mut current_sum = 0;
+        for number in inner_list.iter() {
+            current_sum += number;
+        }
+        group_sums.push(current_sum);
+    }
+    if (group_sums.len() as i32) < *n_groups {
+        return group_sums.iter().sum();
+    }
+    println!("{:?}", &group_sums[240..267]);
+    group_sums.sort();
+    let start_idx = (group_sums.len() as i32) - *n_groups;
+    let mut sum_of_sums = 0;
+    let mut current_idx = start_idx as usize;
+    println!("{:?}", &group_sums[240..267]);
+    while current_idx < (group_sums.len()) {
+        sum_of_sums += group_sums[current_idx];
+        current_idx += 1;
+    }
+
+    return sum_of_sums;
+}
+
 pub struct Config {
     file_path: String,
+    operation: String,
+    n_groups: i32
 }
 
 impl Config {
@@ -74,7 +110,19 @@ impl Config {
             return Err("Expected argument for file_path.");
         }
         let file_path = args[1].clone();
+        let operation;
+        if args.len() > 2 {
+            operation = args[2].clone();
+        } else {
+            operation = "max".to_string();
+        }
+        let n_groups;
+        if args.len() > 3 {
+            n_groups = args[3].clone().parse::<i32>().unwrap();
+        } else {
+            n_groups = 0
+        }
 
-        Ok(Config { file_path })
+        Ok(Config { file_path, operation, n_groups })
     }
 }
