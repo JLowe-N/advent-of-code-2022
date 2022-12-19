04181883705473a80112300bd586e5a83195d33d
Justin Lowen <Justin.G.Lowen@gmail.com>
2022-12-18 22:06:19 -0600

    Day 4 - Part 1: Check for overlapping ranges
    One challenge was ensuring the parsed ranges from split were converted
    to integers properly. The split function can feed into the map
    function to perform that parse
    action.

---
+++ day4/src/lib.rs
@@ -0,0 +1,70 @@
+use std::error::Error;
+use std::fs;
+use std::process;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let input = get_input(&config.file_path).unwrap_or_else(|err| {
+        println!("Application error: {}", err);
+        process::exit(1)
+    });
+    part_one_count_overlaps(&input);
+
+    Ok(())
+}
+
+pub fn get_input(file_path: &str) -> Result<String, Box<dyn Error>> {
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)?;
+
+    Ok(contents)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
+
+pub fn part_one_count_overlaps(input: &str) -> () {
+    let mut overlap_count = 0;
+    for line in input.lines() {
+        if is_overlap(&line) {
+            overlap_count += 1;
+        }
+    }
+    println!("{}", overlap_count);
+}
+
+pub fn is_overlap(range_pair: &str) -> bool {
+    let mut is_overlapping = false;
+    let pairs: Vec<&str> = range_pair.split(',').collect();
+    let left_range: Vec<i32> = pairs[0]
+        .split('-')
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+    let right_range: Vec<i32> = pairs[1]
+        .split('-')
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+    if left_range[0] <= right_range[0] && left_range[1] >= right_range[1] {
+        is_overlapping = true;
+    } else if right_range[0] <= left_range[0] && right_range[1] >= left_range[1] {
+        is_overlapping = true;
+    }
+    is_overlapping
+}
+
+pub struct Config {
+    file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("Expected argument for file_path.");
+        }
+        let file_path = args[1].clone();
+
+        Ok(Config { file_path })
+    }
+}
---
+++ day4/src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::process;
+
+use day4::run;
+use day4::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Program parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
