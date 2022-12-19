027203dd3f62c5e6653a584e274a8c03908d17ec
Justin Lowen <Justin.G.Lowen@gmail.com>
2022-12-18 22:27:54 -0600

    Day 4 - Part 2: Check for partial overlaps
    A slight inversion of the conditions (assume overlapping, check if its
    false) simplifies the condition
    of checking mins/maxes.

--- day4/src/lib.rs
+++ day4/src/lib.rs
@@ -8,6 +8,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         process::exit(1)
     });
     part_one_count_overlaps(&input);
+    part_two_count_overlaps(&input);
 
     Ok(())
 }
@@ -35,6 +36,16 @@ pub fn part_one_count_overlaps(input: &str) -> () {
     println!("{}", overlap_count);
 }
 
+pub fn part_two_count_overlaps(input: &str) -> () {
+    let mut overlap_count = 0;
+    for line in input.lines() {
+        if is_partial_overlap(&line) {
+            overlap_count += 1;
+        }
+    }
+    println!("{}", overlap_count);
+}
+
 pub fn is_overlap(range_pair: &str) -> bool {
     let mut is_overlapping = false;
     let pairs: Vec<&str> = range_pair.split(',').collect();
@@ -50,6 +61,23 @@ pub fn is_overlap(range_pair: &str) -> bool {
         is_overlapping = true;
     } else if right_range[0] <= left_range[0] && right_range[1] >= left_range[1] {
         is_overlapping = true;
+    }
+    is_overlapping
+}
+
+pub fn is_partial_overlap(range_pair: &str) -> bool {
+    let mut is_overlapping = true;
+    let pairs: Vec<&str> = range_pair.split(',').collect();
+    let left_range: Vec<i32> = pairs[0]
+        .split('-')
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+    let right_range: Vec<i32> = pairs[1]
+        .split('-')
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+    if left_range[1] < right_range[0] || right_range[1] < left_range[0] {
+        is_overlapping = false;
     }
     is_overlapping
 }
